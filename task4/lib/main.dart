79eef083fa0d1afd9159b9ee4ebfd5624f05c136
arsema16 <arsema2328@gmail.com>
2025-03-20 09:31:00 +0300

    update

--- task4/lib/main.dart
+++ task4/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Food App',
+      title: 'form practice task',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
